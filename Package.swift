51b25c2d339bc5c1afad67e5e1a2025291a75228
Johannes Weiss <johannesweiss@apple.com>
2020-05-15 22:47:14 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "swift-nio-events-recorder",
     platforms: [
-        .macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS("9999")
+        .macOS(.v10_15), .iOS(.v13), .tvOS(.v13)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
